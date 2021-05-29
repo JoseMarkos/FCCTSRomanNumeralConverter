d5b8223b3b7d33d28132ad9ebbc20c79317f23cb
Marcos <jkmarcosb@gmail.com>
2021-05-29 17:29:07 -0600

    reducing loop iterations

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import convertToRoman from "./convertToRoman.ts";
 (() => {
   let index = 1;
 
-  while (index < 4002) {
+  while (index < 2) {
     console.log(convertToRoman(index));
     index++;
   }
